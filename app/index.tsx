03903b6255a6063b2cec5c8a7ec03fbae1c00443
kotaro1574 <shake1574@gmail.com>
2024-12-03 15:27:36 +0900

    add: Box

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,10 @@
-import { Text, View } from "react-native";
+import { Box } from "@/components/ui/box";
+import { Text } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+    <Box>
       <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    </Box>
   );
 }
